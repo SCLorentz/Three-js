e584f1f30ea98e0ad0ea15839543c5ec67f843e1
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-18 08:22:29 -0300

    Update 3d-2.js

--- src/3d-2.js
+++ src/3d-2.js
@@ -1,29 +1,30 @@
 // Crie uma cena
-var scene = new THREE.Scene();
+const scene = new THREE.Scene(),
+    // other important values for the scene
+    size = 8,                                                                        // Tamanho do grid
+    divisions = 8,                                                                   // Divisões do grid
+    colorCenterLine = new THREE.Color(0x0000ff),                                     // Cor das linhas centrais do grid
+    colorGrid = new THREE.Color(0x808080),                                           // Cor das demais linhas do grid
+    gridHelper = new THREE.GridHelper(size, divisions, colorCenterLine, colorGrid);
 
-const size = 8; // Tamanho do grid
-const divisions = 8; // Divisões do grid
-const colorCenterLine = new THREE.Color(0x0000ff); // Cor das linhas centrais do grid
-const colorGrid = new THREE.Color(0x808080); // Cor das demais linhas do grid
-const gridHelper = new THREE.GridHelper(size, divisions, colorCenterLine, colorGrid);
 gridHelper.name = 'grid'
 
 // Adicione o grid helper à cena
 scene.add(gridHelper);
 
 // Crie uma câmera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 0;
 camera.position.y = 1;
 
-var spherical = new THREE.Spherical();
+const spherical = new THREE.Spherical();
 spherical.setFromVector3(camera.position);
 var playerSphere = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), material);
 playerSphere.name = "player";
 scene.add(playerSphere);
 
 // Crie um objeto pai para a câmera
-var cameraParent = new THREE.Object3D();
+const cameraParent = new THREE.Object3D();
 playerSphere.add(cameraParent);
 
 // Adicione a câmera como filho do objeto pai
@@ -31,10 +32,10 @@ cameraParent.add(camera);
 
 function onMouseMove(event) {
     // Calcula a diferença de posição do mouse em relação ao último frame
-    const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-    const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0,
+        movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0,
     // Define a sensibilidade da rotação
-    const rotationSpeed = 0.002;
+    rotationSpeed = 0.002;
     // Aplica a rotação da câmera com base no movimento do mouse
     playerSphere.rotation.y -= movementX * rotationSpeed;
     camera.rotation.x -= movementY * rotationSpeed;
@@ -44,20 +45,58 @@ function onMouseMove(event) {
 window.addEventListener('mousemove', onMouseMove, false);
 
 // Variáveis para controlar a movimentação e rotação da câmera
-var movementSpeed = 0.1;
-var moveForward = false;
-var moveBackward = false;
-var moveLeft = false;
-var moveRight = false;
-var moveUp = false;
-var moveDown = false;
-
-var rotationSpeed = 0.01;
-var mouseSpeed = 0.002;
+const moveForward = false,
+    moveBackward = false,
+    moveLeft = false,
+    moveRight = false,
+    moveUp = false,
+    moveDown = false,
+    // speed
+    rotationSpeed = 0.01,
+    mouseSpeed = 0.002,
+    movementSpeed = 0.1;
 
 function updatePlayerSphere() {
-    var direction = new THREE.Vector3();
-    if (moveForward) {
+    const direction = new THREE.Vector3();
+    // camera.getWorldDirection(direction)
+
+    const movment = {
+        // front
+        87: () => {
+            // notice that this line is beeing repeted over and over again
+            camera.getWorldDirection(direction);
+            direction.y = 0;
+            //
+            playerSphere.position.add(direction.multiplyScalar(movementSpeed));
+        },
+        // back
+        83: () => {
+            camera.getWorldDirection(direction);
+            direction.y = 0;
+            //
+            playerSphere.position.add(direction.multiplyScalar(-movementSpeed));
+        },
+        // right
+        68: () => {
+            camera.getWorldDirection(direction);
+            const left = new THREE.Vector3(-direction.z, 0, direction.x);
+            //
+            playerSphere.position.add(left.multiplyScalar(movementSpeed));
+        },
+        // left
+        65: () => {
+            camera.getWorldDirection(direction);
+            const right = new THREE.Vector3(direction.z, 0, -direction.x);
+            //
+            playerSphere.position.add(right.multiplyScalar(movementSpeed));
+        },
+        // up
+        32: () => playerSphere.position.y += 0.1,
+        // down
+        16: () => playerSphere.position.y -= 0.1
+    }
+    
+    /*if (moveForward) {
         camera.getWorldDirection(direction);
         direction.y = 0;
         playerSphere.position.add(direction.multiplyScalar(movementSpeed));
@@ -82,12 +121,16 @@ function updatePlayerSphere() {
     }
     if(moveDown) {
         playerSphere.position.y -= 0.1;
-    }
+    }*/
 }
 
 // Adicione os eventos de escuta para as teclas pressionadas
 document.addEventListener('keydown', function (event) {
-switch (event.keyCode) {
+    // o codigo antigo ainda tem uma vantagem que eu posso prever, não posso testar agora pois estou em aula e no editor default do github
+    // mas é possivel de prever que o personagem só será capaz de se mover para uma direção de cada vez.
+    // Ou seja, tentar se mover para a direita e frente ao mesmo tempo não fará o player andar na diagonal, mas sim somente para frente
+    moviment[event.keycode]()?
+/*switch (event.keyCode) {
     case 87: // Tecla W
     moveForward = true;
     break;
@@ -106,9 +149,9 @@ switch (event.keyCode) {
     case 16:
     moveDown = true;
     break;
-}
+}*/
 });
-document.addEventListener('keyup', function (event) {
+/*document.addEventListener('keyup', function (event) {
 switch (event.keyCode) {
     case 87: // Tecla W
     moveForward = false;
@@ -129,14 +172,15 @@ switch (event.keyCode) {
     moveDown = false;
     break;
 }
-});
+});*/
 // Centralize o cursor do mouse inicialmente
 centerMouse();
 
 // Função para centralizar o cursor do mouse
 function centerMouse() {
-    var centerX = window.innerWidth / 2;
-    var centerY = window.innerHeight / 2;
+    const centerX = window.innerWidth / 2,
+         centerY = window.innerHeight / 2;
+    //
     window.dispatchEvent(new MouseEvent('mousemove', {
         clientX: centerX,
         clientY: centerY
@@ -144,10 +188,11 @@ function centerMouse() {
 }
 
 // Crie um renderizador
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x00Afff)
-var canvas = renderer.domElement;
+renderer.setClearColor(0x00Afff);
+//
+const canvas = renderer.domElement;
 document.body.appendChild(renderer.domElement);
 
 // Evento para capturar o mouse
@@ -157,31 +202,34 @@ window.addEventListener('keydown', function () {
 });
 
 // Adicione uma luz direcional à cena
-var light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2, 5, 2);
 scene.add(light);
 
-var textureLoader = new THREE.TextureLoader();
-var texture = textureLoader.load('https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a7/Cobblestone_%28texture%29_JE5_BE3.png');
+// mais valores, observe que são variaveis globais. Tente criar uma função para isso.
+const textureLoader = new THREE.TextureLoader(),
+    texture = textureLoader.load('https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a7/Cobblestone_%28texture%29_JE5_BE3.png'),
 
-const cellSize = 1; // Tamanho da célula do grid
+    cellSize = 1,        // Tamanho da célula do grid
 
-const gridX = 2; // Posição X do grid (em células)
-const gridY = 0; // Posição Y do grid (em células)
-const gridZ = -3; // Posição Z do grid (em células)
+    gridX = 2,           // Posição X do grid (em células)
+    gridY = 0,           // Posição Y do grid (em células)
+    gridZ = -3;          // Posição Z do grid (em células)
 
 // Calcule as coordenadas do bloco com base no grid
 const blockX = gridX * cellSize - 0.5;
 const blockY = gridY * cellSize;
 const blockZ = gridZ * cellSize - 0.5;
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshPhongMaterial({ map: texture });
-var cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1),
+    material = new THREE.MeshPhongMaterial({ map: texture }),
+    cube = new THREE.Mesh(geometry, material);
+//
 cube.interactive = true;
 cube.position.set(blockX, blockY, blockZ);
 scene.add(cube);
 
+// ok, tenho 99% de certeza que tem uma forma melhor de fazer isso, seja lá o que for
 for(let j = 0; j<5; j++) {
     for(let i = 1; i<12; i++) {
         var clonedObject = cube.clone();
@@ -190,9 +238,10 @@ for(let j = 0; j<5; j++) {
     }
 }
 
-var geometry2 = new THREE.BoxGeometry(20, 1, 20);
-var material2 = new THREE.MeshPhongMaterial({ color: 0xff0f00 });
-var cube2 = new THREE.Mesh(geometry2, material2);
+const geometry2 = new THREE.BoxGeometry(20, 1, 20),
+    material2 = new THREE.MeshPhongMaterial({ color: 0xff0f00 }),
+    cube2 = new THREE.Mesh(geometry2, material2);
+//
 cube2.position.set(0, -1, 0);
 cube2.name = "chão";
 scene.add(cube2);
@@ -237,24 +286,24 @@ function onMouseClick(event) {
     canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
     canvas.requestPointerLock();
     // Definir o raio de projeção a partir da posição da câmera
-    var raycaster = new THREE.Raycaster();
+    const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
 
     // Verificar objetos intersectados pelo raio, ignorando os objetos especificados
-    var intersects = raycaster.intersectObjects(scene.children, true).filter(function (intersection) {
-        return intersection.object.name !== 'player' && intersection.object.name !== 'grid';;
+    const intersects = raycaster.intersectObjects(scene.children, true).filter(function (intersection) {
+        return intersection.object.name != 'player' && intersection.object.name != 'grid';
     });
 
     // Verificar se houve interseção com algum objeto
     if (intersects.length > 0) {
         // Objeto 3D clicado
-        var clickedObject = intersects[0].object;
+        const clickedObject = intersects[0].object;
         // Faça algo com o objeto clicado
         console.log('Objeto 3D clicado:', clickedObject);
-        if (clickedObject.name !== "chão") {
-            scene.remove(clickedObject);
-            breakBlock.play();
-        }
+        if (clickedObject.name == "chão") return
+        //
+        scene.remove(clickedObject);
+        breakBlock.play();
     }
 }
 document.addEventListener('click', onMouseClick, false);
